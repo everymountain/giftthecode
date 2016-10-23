888b5e4bf9a535c0cead3881243a44d9f1ba4b05
Dave MacDonald <mindlapse@gmail.com>
2016-10-22 21:04:46 -0400

    Hello world

--- node/public/js/main.js
+++ node/public/js/main.js
@@ -1,4 +1,13 @@
 var url = 'http://localhost/api';
+var log = console.log;
+
+
+var Keys = {
+    channelId : 'UCMABz0K2kvC7YF6GwYfDB0w',
+    playlistId : 'PLD475BA4B0793FF8B',
+    youtubeApiKey : 'AIzaSyBWoJsAtQVYq60JIrSvg32GoHonnfKqiE8'
+}
+
 document.getElementById("bgVideo").playbackRate = 1.25;
 
 // Youtube api: AIzaSyBWoJsAtQVYq60JIrSvg32GoHonnfKqiE8
@@ -31,10 +40,12 @@ angular.module("EveryMountain", [])
     var self = this;
 
     self.world = "World"
-    self.cards = mockdata;
+    self.cards = [];
+
+
 
     self.getCardUrl = function(card) {
-        return card.url + "?rel=0&autoplay=1&loop=1";
+        return card.url + "?rel=0&autoplay=0&loop=1";
     }
 
     self.getCardVolume = function(card) {
@@ -48,41 +59,123 @@ angular.module("EveryMountain", [])
                 $("#" + card.id).mute();
             }
         );
-    }
-    //
-    // for (var i = 0; i < self.cards.length; i++) {
-    //     //$("#"+self.cards[i].id)[0].mute();
-    // }
+    };
 
-    $http.put(url + '/myindex/mytype/12345', {abc:1, b:2}).then(function(res) {
-        console.dir(res);
+    var videosLoaded = 0;
 
+    (function getCards(pageToken) {
 
-        // Example of a search
+        var url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2Csnippet&playlistId=" +
+            Keys.playlistId +
+            "&key=" +
+            Keys.youtubeApiKey;
 
-        $http.post(url + '/search', {
-            index : 'myindex',
-            body: {
-                query: {
-                    match: {
-                        abc: 123
-                    }
-                }
+        if (pageToken) {
+            url += "&pageToken=" + pageToken;
+        }
+
+        $http.get(url).then(function(response) {
+            log(response.data);
+            if (!response || !response.data) {
+                return
             }
-        }).then(function(response) {
-            response.data.forEach(function(item) {
-                console.log("search result", item);
-            });
+            var pageToken = response.data.nextPageToken;
+            var items =  response.data.items;
+
+            if (items) {
+                items.forEach(function(item) {
+                    console.log(item);
+                    if (item.kind != "youtube#playlistItem") {
+                        return;
+                    }
+                    var card = {
+                        url : 'https://www.youtube.com/embed/' + item.contentDetails.videoId,
+                        name : item.snippet.title,
+                        bio : item.snippet.description
+                    };
+                    log(item.contentDetails.videoId);
+
+                    self.cards.push(card)
+                });
+
+                console.log(pageToken);
+                if (pageToken) {
+                    getCards(pageToken);
+                }
+
+            }
+
         });
+    })();
 
 
-        //
-        // Example of fetching one record by id:
-        //
-        $http.get(url + '/myindex/mytype/12345').then(function(res) {
-            console.log("item fetched:", res.data);
-        })
 
-    })
+    // $http.put(url + '/myindex/mytype/12345', {abc:1, b:2}).then(function(res) {
+    //     console.dir(res);
+    //
+    //
+    //     // Example of a search
+    //
+    //     $http.post(url + '/search', {
+    //         index : 'myindex',
+    //         body: {
+    //             query: {
+    //                 match: {
+    //                     abc: 123
+    //                 }
+    //             }
+    //         }
+    //     }).then(function(response) {
+    //         response.data.forEach(function(item) {
+    //             console.log("search result", item);
+    //         });
+    //     });
+    //
+    //
+    //     //
+    //     // Example of fetching one record by id:
+    //     //
+    //     $http.get(url + '/myindex/mytype/12345').then(function(res) {
+    //         console.log("item fetched:", res.data);
+    //     })
+    //
+    // })
+    //
+    //
+    // //
+    // // for (var i = 0; i < self.cards.length; i++) {
+    // //     //$("#"+self.cards[i].id)[0].mute();
+    // // }
+    //
+    // $http.put(url + '/myindex/mytype/12345', {abc:1, b:2}).then(function(res) {
+    //     console.dir(res);
+    //
+    //
+    //     // Example of a search
+    //
+    //     $http.post(url + '/search', {
+    //         index : 'myindex',
+    //         body: {
+    //             query: {
+    //                 match: {
+    //                     abc: 123
+    //                 }
+    //             }
+    //         }
+    //     }).then(function(response) {
+    //         response.data.forEach(function(item) {
+    //             console.log("search result", item);
+    //         });
+    //     });
+    //
+    //
+    //     //
+    //     // Example of fetching one record by id:
+    //     //
+    //     $http.get(url + '/myindex/mytype/12345').then(function(res) {
+    //         console.log("item fetched:", res.data);
+    //     })
+    //
+    // })
 
 }])
